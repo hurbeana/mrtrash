28f845bdb0f444cd838ef189dd54ef18820284f7
hurbeana <hurbeana@gmail.com>
2019-05-04 19:30:14 +0200

    removed unused references

--- app/src/main/java/at/mrtrash/MainActivity.kt
+++ app/src/main/java/at/mrtrash/MainActivity.kt
@@ -1,25 +1,12 @@
 package at.mrtrash
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import at.mrtrash.models.Wasteplace
-import at.mrtrash.network.DataService
-import at.mrtrash.network.WasteplaceResponse
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
