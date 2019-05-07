5ca55dd75d132fe7df3b305112cdaa6fbd597e0c
hurbeana <hurbeana@gmail.com>
2019-05-07 23:47:43 +0200

    added vuforia support and object recognition

--- app/src/main/java/at/mrtrash/MainActivity.kt
+++ app/src/main/java/at/mrtrash/MainActivity.kt
@@ -9,6 +9,21 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import android.widget.Toast
+import android.view.MenuItem
+import android.view.Menu
+import androidx.core.content.ContextCompat.startActivity
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import at.mrtrash.vuforia.ObjectTargetRenderer
+import at.mrtrash.vuforia.ObjectTargets
+import android.app.Activity
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
+import at.mrtrash.fragments.WasteTypeFragmentDirections
+import at.mrtrash.models.wasteType.WasteTypeListViewModel
+import at.mrtrash.utils.InjectorUtils
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,11 +54,24 @@ class MainActivity : AppCompatActivity() {
                 1
             )
         }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(
+                    android.Manifest.permission.CAMERA
+                ),
+                1
+            )
+        }
     }
 
     private fun initViewModels() {
         //TODO: maybe init this here so that data is only loaded once.. Attention with location permissions!
         //ViewModelProviders.of(this, DisposalOptionViewModelFactory(application)).get(DisposalOptionViewModel::class.java)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
